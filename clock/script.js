aa0bda0cd3a9ec8c2110d3f10653aa2e436ceb09
joeym-09 <anniehuynh.m@gmail.com>
2024-05-21 17:16:44 -0400

    clock

---
+++ clock/script.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector('.second-hand')
+const minsHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
+
+function setDate() {
+  const currentTime = new Date()
+
+  const seconds = currentTime.getSeconds()
+  const secondDeg = (seconds / 60) * 360 + 90
+  secondHand.style.transform = `rotate(${secondDeg}deg)`
+
+  const mins = currentTime.getMinutes()
+  const minsDeg = (mins / 60) * 360 + 90
+  minsHand.style.transform = `rotate(${minsDeg}deg)`
+
+  const hours = currentTime.getHours()
+  const hoursDeg = (hours / 12) * 360 + 90
+  hourHand.style.transform = `rotate(${hoursDeg}deg)`
+
+  requestAnimationFrame(setDate)
+}
+setDate()
